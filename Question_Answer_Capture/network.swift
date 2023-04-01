143f98a32167d26a607bf944e0d3b1f2812190ca
vijayaraj <vijayarajsuyambu@gmail.com>
2023-04-01 18:50:28 +0400

    Initial design and implementation

---
+++ Question_Answer_Capture/network.swift
@@ -0,0 +1,19 @@
+//
+//  network.swift
+//  Question_Answer_Capture
+//
+//  Created by Web_Dev on 4/1/23.
+//
+
+import Foundation
+
+
+struct QADetail:Codable {
+   
+    var Category:String
+    var Question:String
+    var Answer:String
+
+
+}
+
